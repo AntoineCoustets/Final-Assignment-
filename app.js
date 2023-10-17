07acb5991276c2a36c76795eb2c052660b3acbdb
Homesick-Alien <rapha.llorca@gmail.com>
2023-10-16 19:06:46 -0700

    Integrate Vue components to express app

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static('client/dist'));
 
 // Your routes will go here
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+  });
+  
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
